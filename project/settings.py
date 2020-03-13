45fd82fbb10b6786ea26a6eca821e04721d8c9bf
jecinta <jecintawanjirug@gmail.com>
2020-03-13 11:38:05 +0300

    this adds the static url in the settings

--- project/settings.py
+++ project/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'awards',
-    'bootstrap4'
+    'bootstrap4',
+    'tinymce'
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
